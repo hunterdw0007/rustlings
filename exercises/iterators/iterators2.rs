6ebf8546359afe01ae75cd16d831fa55d1b2b879
hunterdw0007 <hunterw.mail@gmail.com>
2023-01-21 21:18:21 -0600

    fix: iterators exercises 😵‍💫 🧠

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -3,7 +3,6 @@
 // can offer. Follow the steps to complete the exercise.
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 // Step 1.
 // Complete the `capitalize_first` function.
@@ -12,8 +11,9 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => format!("{}{}", first.to_ascii_uppercase(), c.as_str()),
     }
+	
 }
 
 // Step 2.
@@ -21,7 +21,11 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+	let mut out: Vec<String> = vec![];
+	for word in words {
+		out.push(capitalize_first(word));
+	}
+	out
 }
 
 // Step 3.
@@ -29,7 +33,7 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    words.iter().map(|&x| capitalize_first(x)).collect()
 }
 
 #[cfg(test)]
--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -1,7 +1,6 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -13,6 +12,21 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+	
+	//NOTE: Initial Version with for loop
+	/*
+	let mut factorial: u64 = 1;
+	
+	for i in (1..=num).rev() {
+		factorial *= i;
+	}	
+
+	factorial
+	*/
+	
+	let range: Vec<u64> = (1..=num).collect();
+
+	range.iter().fold(1, |acc, &x| acc * x)	
 }
 
 #[cfg(test)]
